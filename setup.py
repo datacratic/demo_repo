5f858fa2a3c3275334c93d9a0bb2c2dbe5890db9
alex <alex@datacratic.com>
2012-08-10 14:52:49 -0400

    changes to forse pip install from git

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 setup(name='demo_repo',
       version='1.0',
       packages = ['package'],
-      py_modules=['runner'],
+      install_requires = ['setuptools'],
       )
